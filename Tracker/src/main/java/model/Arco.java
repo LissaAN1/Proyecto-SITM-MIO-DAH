6f257ac62cfb17173338e6f639dbcc3274d6e1a6
Angy <hangiemaria@gmail.com>
2025-11-12 00:40:56 -0500

    fCase PArco con atributos básicos

--- Tracker/src/main/java/model/Arco.java
+++ Tracker/src/main/java/model/Arco.java
@@ -1,24 +1,24 @@
 package model;
-
 public class Arco {
-    private Parada inicio;
-    private Parada fin;
+    private final String rutaId;
+    private final int orientation;
+    private final Parada origen;
+    private final Parada destino;
+    private final double distanciaKm;
 
-    public Arco(Parada inicio, Parada fin) {
-        this.inicio = inicio;
-        this.fin = fin;
-    }
-
-    public Parada getInicio() {
-        return inicio;
-    }
-
-    public Parada getFin() {
-        return fin;
+    public Arco(String rutaId, int orientation, Parada origen, Parada destino, double distanciaKm) {
+        this.rutaId = rutaId;
+        this.orientation = orientation;
+        this.origen = origen;
+        this.destino = destino;
+        this.distanciaKm = distanciaKm;
     }
 
     @Override
     public String toString() {
-        return inicio.getNombre() + " -> " + fin.getNombre();
+        String sentido = (orientation == 0) ? "IDA" : "REGRESO";
+        return "Ruta: " + rutaId + " (Sentido " + sentido + ") | " +
+                origen.getShortName() + " -> " + destino.getShortName() +
+                " | Distancia: " + String.format("%.3f", distanciaKm) + " km";
     }
 }
